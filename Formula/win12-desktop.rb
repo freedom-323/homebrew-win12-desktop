a1a3043fe2362172a085004ce72a2fd113b006ed
freedom-323 <210706743+freedom-323@users.noreply.github.com>
2026-09-20 18:32:11 +0800

    v0.2.9

--- Formula/win12-desktop.rb
+++ Formula/win12-desktop.rb
@@ -1,20 +1,20 @@
 class Win12Desktop < Formula
   desc "Win12 Desktop"
   homepage "https://github.com/win12-online/win12-desktop"
+  version "0.2.9"  # ***
   license "EPL-2.0"
-  version "0.2.7"  # ***
 
   if OS.mac?
     url "https://github.com/win12-online/win12-desktop.git",
         tag:      "v#{version}",
-        revision: "29a4d2f73ba45d7d9a7b95aa45fb47caa53fc458"  # ***
+        revision: "3c8eeee10e6096a05a535e0cd1f4edca2be39c61"  # ***
 
     depends_on "node" => :build
     depends_on "pnpm" => :build
     depends_on "rust" => :build
   elsif OS.linux?
     url "https://github.com/win12-online/win12-desktop/releases/download/v#{version}/Win12_#{version}_amd64.AppImage"
-    sha256 "65325a26d969bc531957b0a44be6e9ce2a3fb33044146bf0fe51bd8ec9082b82"  # ***
+    sha256 "a1f471cdb3787d34bee6402508914f2f75b7009852ac5607c8e329757856ee6c"  # ***
   end
 
   def install
